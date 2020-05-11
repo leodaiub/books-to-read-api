d99f5498206e6eb1539a3ff3da0303e13b6b2468
Leonardo <leodaiub@gmail.com>
2020-05-11 15:00:40 -0300

    category crud

---
+++ app/Controllers/Http/CategoryController.js
@@ -0,0 +1,83 @@
+"use strict";
+
+/** @typedef {import('@adonisjs/framework/src/Request')} Request */
+/** @typedef {import('@adonisjs/framework/src/Response')} Response */
+/** @typedef {import('@adonisjs/framework/src/View')} View */
+const Category = use("App/Models/Category");
+
+/**
+ * Resourceful controller for interacting with categories
+ */
+class CategoryController {
+  /**
+   * Show a list of all categories.
+   * GET categories
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async index({ request, response, view }) {
+    const categories = await Category.all();
+    return categories;
+  }
+
+  /**
+   * Create/save a new category.
+   * POST categories
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async store({ request, response }) {
+    const category = await Category.create(request.all());
+    return category;
+  }
+
+  /**
+   * Display a single category.
+   * GET categories/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   * @param {View} ctx.view
+   */
+  async show({ params, request, response, view }) {
+    const category = await Category.find(params.id);
+    return category;
+  }
+
+  /**
+   * Update category details.
+   * PUT or PATCH categories/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async update({ params, request, response }) {
+    const category = await Category.findOrFail(params.id);
+    category.merge(request.all());
+    await category.save();
+    return category;
+  }
+
+  /**
+   * Delete a category with id.
+   * DELETE categories/:id
+   *
+   * @param {object} ctx
+   * @param {Request} ctx.request
+   * @param {Response} ctx.response
+   */
+  async destroy({ params, request, response }) {
+    const category = await Category.findOrFail(params.id);
+    await category.delete();
+    return category;
+  }
+}
+
+module.exports = CategoryController;
